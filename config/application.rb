dc766f48767c0ef60d06f7d34a2922ebb5f57547
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-28 20:43:49 +0900

    Update config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,14 @@ module SampleJohn
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # アプリケーションでサポートする言語の一覧を設定
+    config.i18n.available_locales = %i[ja en]
+    # デフォルトのlocaleを:ja(日本語にする)
+    config.i18n.default_locale = :ja
+    # ロケールファイル読み込みのpathを通す
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    # 基準時刻を日本時間に設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
