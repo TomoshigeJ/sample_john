57791ebe60525f0c8536b8465666cc8a879fe2a6
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-04-02 17:52:38 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'users#index'
-  resources :users	
+  resources :users
+
+  resources :animals do
+    resources :comments
+  end
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
